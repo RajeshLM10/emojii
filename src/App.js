ddd35bf30bfdd006d668042e21ca0086e868fa2a
RajeshLM10 <95027139+RajeshLM10@users.noreply.github.com>
2023-12-04 12:16:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import EmojiQuote from './EmojiQuote';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <EmojiQuote />
+      </header>
+    </div>
+  );
+}
+
+export default App;
